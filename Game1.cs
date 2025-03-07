7467e516fd0165d1c2a0df35c33d198d9648dfd0
TOP-FDesmet <florian.desmet@topchretien.com>
2025-03-07 15:26:08 +0100

    create Hero

--- Game1.cs
+++ Game1.cs
@@ -8,8 +8,6 @@ namespace FirstGame;
 public class Game1 : Game
 {
     private Hero hero;
-    float ballSpeed;
-    int deadZone;
 
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
@@ -24,8 +22,6 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        ballSpeed = 100f;
-        deadZone = 4096;
 
         base.Initialize();
     }
@@ -47,66 +43,7 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
-        float updateBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        var kstate = Keyboard.GetState();
-
-        /* if (kstate.IsKeyDown(Keys.Up))
-        {
-            ballPosition.Y -= updateBallSpeed;
-        }
-        if (kstate.IsKeyDown(Keys.Down))
-        {
-            ballPosition.Y += updateBallSpeed;
-        }
-        if (kstate.IsKeyDown(Keys.Left))
-        {
-            ballPosition.X -= updateBallSpeed;
-        }
-        if (kstate.IsKeyDown(Keys.Right))
-        {
-            ballPosition.X += updateBallSpeed;
-        }
-
-        if (Joystick.LastConnectedIndex == 0)
-        {
-            JoystickState jstate = Joystick.GetState((int)PlayerIndex.One);
-
-            if (jstate.Axes[1] < -deadZone)
-            {
-                ballPosition.Y -= updateBallSpeed;
-            }
-            if (jstate.Axes[1] > deadZone)
-            {
-                ballPosition.Y += updateBallSpeed;
-            }
-            if (jstate.Axes[0] < -deadZone)
-            {
-                ballPosition.X -= updateBallSpeed;
-            }
-            if (jstate.Axes[0] > deadZone)
-            {
-                ballPosition.X += updateBallSpeed;
-            }
-        }
-
-        if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
-        {
-            ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
-        }
-        else if (ballPosition.X < ballTexture.Width / 2)
-        {
-            ballPosition.X = ballTexture.Width / 2;
-        }
-
-        if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
-        {
-            ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
-        }
-        else if (ballPosition.Y < ballTexture.Height / 2)
-        {
-            ballPosition.Y = ballTexture.Height / 2;
-        } */
+        hero.Move(gameTime, _graphics);
 
         base.Update(gameTime);
     }
@@ -118,16 +55,6 @@ public class Game1 : Game
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
         hero.Draw(_spriteBatch);
-        /* _spriteBatch.Draw(
-            ballTexture,
-            ballPosition,
-            null,
-            Color.White,
-            0f,
-            new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
-            Vector2.One,
-            SpriteEffects.None,
-            0f); */
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- Sprite.cs
+++ Sprite.cs
@@ -8,20 +8,13 @@ public abstract class Sprite : ISprite
   public Texture2D Texture { get; set; }
   public float PositionX { get; set; }
   public float PositionY { get; set; }
-  private Vector2 Position { get; set; }
+  public Vector2 Position { get; protected set; }
 
   public Sprite(Texture2D texture, float positionX, float positionY)
   {
     Texture = texture;
     PositionX = positionX;
     PositionY = positionY;
-    Position = new(PositionX, PositionY);
-  }
-
-  protected Sprite(Texture2D texture, Vector2 position)
-  {
-    Texture = texture;
-    Position = position;
   }
 
   public void Draw(SpriteBatch spriteBatch)
